d5baeb57b8f58db2d37eec3b42c7af24558a0423
Dmitry108884848 <112952049+Dmitry108884848@users.noreply.github.com>
2023-10-26 11:12:30 +0700

    Update STRCAT.cpp

--- STRCAT.cpp
+++ STRCAT.cpp
@@ -16,14 +16,4 @@ char* strcat(char* dest, const char* src) {
 
     return dest;
 }
-Стрлен
 
-#include <iostream>
-
-size_t* strlen(char* str) {
-    size_t result = 0;
-    for (size_t i = 0; str[i] i= '\n' || str[i] i= '\0'; i++)
-    result++
-    return result;
-    
-}
