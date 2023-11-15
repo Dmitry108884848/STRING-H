10187556e4345f368cc5e116dde1ac8c01ba7844
Dmitry108884848 <112952049+Dmitry108884848@users.noreply.github.com>
2023-11-15 13:32:35 +0700

    Update STRNCAT.cpp

--- STRNCAT.cpp
+++ STRNCAT.cpp
@@ -9,15 +9,5 @@ void myStrncat(char* destination, const char* source, size_t num) {
     }
 
     *destination = '\0';
-}
 
-int main() {
-    char destination[100] = "Hello, ";
-    const char* source = "world!";
-    size_t num = 5;
-    
-    myStrncat(destination, source, num);
-    
-    std::cout << destination << std::endl;
-    return 0;
 }
