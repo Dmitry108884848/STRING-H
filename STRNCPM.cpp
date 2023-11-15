64d3092b71d9ee6d7a0e108079cebe36d27a5e75
Dmitry108884848 <112952049+Dmitry108884848@users.noreply.github.com>
2023-11-15 13:33:00 +0700

    Update STRNCPM.cpp

--- STRNCPM.cpp
+++ STRNCPM.cpp
@@ -12,37 +12,3 @@ int my_strncmp(const char* str1, const char* str2, size_t n) {
 }
    
 
-int main() {
-    const char* str1 = "apple";
-    const char* str2 = "banana";
-    size_t n = 3;
-
-
-    int result = my_strcmp(str1, str2);
-
-    if (result == 0) {
-        std::cout << "Строки идентичны" << std::endl;
-    }
-    else if (result < 0) {
-        std::cout << "Первая строка меньше второй" << std::endl;
-    }
-    else {
-        std::cout << "Вторая строка меньше первой" << std::endl;
-    }
-
-    int result = my_strncmp(str1, str2, n);
-
-    if (result == 0) {
-        std::cout << "Первые " << n << " символа идентичны" << std::endl;
-    }
-    else if (result < 0) {
-        std::cout << "Первые " << n << " символа первой строки меньше второй" << std::endl;
-    }
-    else {
-        std::cout << "Первые " << n << " символа второй строки меньше первой" << std::endl;
-    }
-
-
-
-    return 0;
-}
